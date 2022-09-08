4b3171a4a590d95468853748c3e948c1d5c4427f
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-08 19:02:52 -0300

    Starting the submit configuration

--- contact-form-configuration.js
+++ contact-form-configuration.js
@@ -1,5 +1,28 @@
-let contactServiceInput = document.getElementById("Service");
+$(document).ready(function () {
+    let contactServiceInput = document.getElementById("Service");
+    const $contactSubmit = $("#contact-submit");
+});
+
+let openBookMeetingPage = true;
 
 function SetContactService(serviceName) {
     contactServiceInput.value = serviceName;
 }
+
+function JustSendForm() {
+    openBookMeetingPage = false;
+    $contactSubmit.click();
+}
+
+$contactSubmit.submit(function (e) {
+    // If the user wants to book a meeting open the page on a new tab
+    if(openBookMeetingPage) {
+        window.open('/book-meeting', '_blank');
+    }
+    // Reset the variable in case the user wants to send another message
+    else {
+        openBookMeetingPage = true;
+    }
+    // Submit the form
+    return true;
+});
