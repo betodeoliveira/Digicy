d54da92cb7785bf3aab3f48fd847058a7a85fc66
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-08 19:07:56 -0300

    Working on contact buttons

--- contact-form-configuration.js
+++ contact-form-configuration.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
     let contactServiceInput = document.getElementById("Service");
+    let openBookMeetingPage = true;
     const $contactSubmit = $("#contact-submit");
     const $contactSubmitAndBook = $("#contact-submit-and-book");
-});
 
-let openBookMeetingPage = true;
-
-function SetContactService(serviceName) {
-    contactServiceInput.value = serviceName;
-}
-
-$contactSubmit.click(function (e) {
-    openBookMeetingPage = false;
-    $contactSubmit.click();
-});
-
-$contactSubmit.submit(function (e) {
-    // If the user wants to book a meeting open the page on a new tab
-    if(openBookMeetingPage) {
-        window.open('/book-meeting', '_blank');
+    function SetContactService(serviceName) {
+        contactServiceInput.value = serviceName;
     }
-    // Reset the variable in case the user wants to send another message
-    else {
-        openBookMeetingPage = true;
-    }
-    // Submit the form
-    return true;
+    
+    $contactSubmit.click(function (e) {
+        openBookMeetingPage = false;
+        $contactSubmit.click();
+    });
+    
+    $contactSubmit.submit(function (e) {
+        // If the user wants to book a meeting open the page on a new tab
+        if(openBookMeetingPage) {
+            window.open('/book-meeting', '_blank');
+        }
+        // Reset the variable in case the user wants to send another message
+        else {
+            openBookMeetingPage = true;
+        }
+        // Submit the form
+        return true;
+    });
 });
