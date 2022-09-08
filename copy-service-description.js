02f65034ea2cd609fa15b57ebf1bda6e687a7f81
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-08 12:55:57 -0300

    Function to copy the digiral solutions description

--- copy-service-description.js
+++ copy-service-description.js
@@ -24,3 +24,30 @@ function UpdateWebdesignServiceDescription(elemento) {
         }
     }
 } 
+
+function UpdateDigitalServiceDescription(elemento) {
+    // Gets the element that will receive the content about the job offer
+    let digitalServiceDescription = document.getElementById("digital-service-description");
+    // The custom code is placed inside an embed objt so we select it
+    let elementParent = elemento.parentNode;
+    // Then we select the select service wrapper
+    elementParent = elementParent.parentNode;
+    // Then we select the buttons wrapper parent
+    elementParent = elementParent.parentNode;
+    // Then we select the parent of the entire cms item
+    elementParent = elementParent.parentNode;
+    // We find the element that has the content about the job offer
+    for (var i = 0; i < elementParent.childNodes.length; i++) {
+        // console.log(elementParent.childNodes[i].className);
+        if (
+            elementParent.childNodes[i].className ==
+            "rich-text-services is-hide w-richtext"
+        ) {
+            // Now we wait the description animation and update the element content
+            setTimeout(function() {
+                digitalServiceDescription.innerHTML = elementParent.childNodes[i].innerHTML;
+            }, 250);
+            break;
+        }
+    }
+} 
