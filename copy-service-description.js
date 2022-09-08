9e04ced5d3bd017e86903ee0128cb7a6dd6a775d
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-08 11:18:27 -0300

    New line when selection the description parent

--- copy-service-description.js
+++ copy-service-description.js
@@ -3,6 +3,8 @@ function UpdateWebdesignServiceDescription(elemento) {
     let webdesignServiceDescription = document.getElementById("webdesign-service-description");
     // The custom code is placed inside an embed objt so we select it
     let elementParent = elemento.parentNode;
+    // Then we select the buttons wrapper parent
+    elementParent = elementParent.parentNode;
     // Then we select the parent of the entire cms item
     elementParent = elementParent.parentNode;
     // We find the element that has the content about the job offer
