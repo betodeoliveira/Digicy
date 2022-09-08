159749f0a4eb1020544b3a93807a0ef885eb0de8
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-08 09:42:47 -0300

    First deploy

--- testimonials-gsap.js
+++ testimonials-gsap.js
@@ -1,1 +1,57 @@
+// © Code by T.RICKS, https://www.timothyricks.com/
+// Copyright 2021, T.RICKS, All rights reserved.
+// You have the license to use this code in your projects but not to redistribute it to others
+gsap.registerPlugin(ScrollTrigger);
+let horizontalItem = $(".testimonial_item");
+let horizontalSection = $(".testimonials");
+let moveDistance;
+function calculateScroll() {
+  // Desktop
+  let itemsInView = 2.5;
+  let scrollSpeed = 0.8;
 
+  if (window.matchMedia("(max-width: 479px)").matches) {
+    // Mobile Portrait
+    itemsInView = 1;
+    scrollSpeed = 1.2;
+  } else if (window.matchMedia("(max-width: 767px)").matches) {
+    // Mobile Landscape
+    itemsInView = 1;
+    scrollSpeed = 1.2;
+  } else if (window.matchMedia("(max-width: 991px)").matches) {
+    // Tablet
+    itemsInView = 2;
+    scrollSpeed = 1.2;
+  }
+  let moveAmount = horizontalItem.length - itemsInView;
+  let minHeight =
+    scrollSpeed * horizontalItem.outerWidth() * horizontalItem.length;
+  if (moveAmount <= 0) {
+    moveAmount = 0;
+    minHeight = 0;
+    // horizontalSection.css('height', '100vh');
+  } else {
+    horizontalSection.css("height", "200vh");
+  }
+  moveDistance = horizontalItem.outerWidth() * moveAmount;
+  horizontalSection.css("min-height", minHeight + "px");
+}
+calculateScroll();
+window.onresize = function () {
+  calculateScroll();
+};
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".testimonial_trigger",
+    // trigger element - viewport
+    start: "top top",
+    end: "bottom top",
+    invalidateOnRefresh: true,
+    scrub: 1
+  }
+});
+tl.to(".testimonials .testimonial_list", {
+  x: () => -moveDistance,
+  duration: 1
+});
