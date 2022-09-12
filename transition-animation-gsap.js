ba3971ab60a05c1a559ca9057a548d1b8dced89e
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-12 16:07:45 -0300

    Transition

--- transition-animation-gsap.js
+++ transition-animation-gsap.js
@@ -3,7 +3,7 @@ let tlTransitionShow = gsap.timeline();
 tlTransitionShow.pause();
 tlTransitionShow.set(".transition", {display: "block"})
 .fromTo(".transition_content", {opacity: 1}, {opacity: 0, duration: 0.5})
-.fromTo("#path", {drawSVG: "0%"}, {drawSVG: "100%", duration: 10, delay: 1}, 0.1)
+.fromTo("#path", {drawSVG: "0%"}, {drawSVG: "100%", duration: 2, delay: 1}, 0.1)
 .fromTo(".transition_logo-white", {opacity: 1}, {opacity: 0, duration: 0.5}, "<1")
 .fromTo(".transition_logo-black", {opacity: 0}, {opacity: 1, duration: 0.5}, "<");
 
