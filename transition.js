8277bc34d78cf676c56c15ba5986ed775acd3fdc
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-12 15:05:30 -0300

    temp

--- transition.js
+++ transition.js
@@ -1,4 +1,9 @@
 gsap.registerPlugin(DrawSVGPlugin);
-
 let tl2 = gsap.timeline();
-tl2.fromTo("#path", {drawSVG: 0}, {durantion: 1, drawSVG: 1});
+tl2.fromTo("#path", {drawSVG: "0%"}, {drawSVG: "100%", duration: 4});
+tl2.fromTo(".transition_logo-white", {opacity: 1}, {opacity: 0, duration: 0.5});
+tl2.fromTo(".transition_logo-black", {opacity: 0}, {opacity: 1, duration: 0.5}, "<");
+
+
+
+
