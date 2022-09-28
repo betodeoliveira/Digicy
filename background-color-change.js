40a2bcb75ed3695a59a52d6438ec9836885ee698
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-28 16:51:28 -0300

    Background Color

--- background-color-change.js
+++ background-color-change.js
@@ -19,30 +19,15 @@ $(".background-trigger").each(function (index) {
     });
 });
 
+let resizeTimeoutId;
 
-const resizeObserver = new ResizeObserver(entries => 
-    console.log('Body height changed')
-  );
+const resizeObserver = new ResizeObserver(entries => {
+    // console.log('Body height changed');
+    clearTimeout(resizeTimeoutId);
+    resizeTimeoutId = setTimeout(() => {
+        ScrollTrigger.refresh();
+    }, 200);
+});
 
   // start observing a DOM node
 resizeObserver.observe(document.body);
-
-// When the page is done we need to wait the trasition animation to end before placing the triggers on the page
-$(document).ready(function () {
-    setTimeout(function(){
-        ScrollTrigger.refresh();
-    }, 2500);
-});
-
-// Updates the background triggers position
-$("#portfolio-loader-more").on( "click", function() {
-    setTimeout(function(){
-        ScrollTrigger.refresh();
-    }, 1000);
-});
-
-$(".services_select-service, .button-circle").on( "click", function() {
-    setTimeout(function(){
-        ScrollTrigger.refresh();
-    }, 600);
-});
